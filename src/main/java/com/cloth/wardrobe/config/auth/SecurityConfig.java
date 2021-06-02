b9667a71c44e7ad2c49f71a81f3d609968847d72
leeyunbo <servers1@naver.com>
2021-06-02 23:23:02 +0900

    [개발]spring security 설정 구현

---
+++ src/main/java/com/cloth/wardrobe/config/auth/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.cloth.wardrobe.config.auth;
+
+import com.cloth.wardrobe.domain.member.MemberAuthority;
+import com.cloth.wardrobe.service.CustomOAuth2MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+//Spring Security 설정 활성화
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOAuth2MemberService customOAuth2MemberService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                    .authorizeRequests() // URL별 권한 관리를 설정하는 옵션의 시작점
+                    .antMatchers("/", "/css/**", "/images/**", "/js/**").permitAll() // 전체 열람 권한
+                    .antMatchers("/api/v1/**").hasRole(MemberAuthority.USER.name()) // USER 권한 사용자만 접근 가능하게 구현
+                    .anyRequest().authenticated()  // 나머지 URL은 authenticate 즉, 로그인된 사용자만 접근 가능
+                .and()
+                    .logout()
+                        .logoutSuccessUrl("/") // 로그아웃 성공시 "/"로 이동
+                .and()
+                    .oauth2Login()
+                        .userInfoEndpoint()
+                            .userService(customOAuth2MemberService); //로그인 성공시 후속조치를 할 Service 등록
+    }
+}
