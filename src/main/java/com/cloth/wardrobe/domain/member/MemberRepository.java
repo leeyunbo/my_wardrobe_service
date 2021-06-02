2d995f621052dec7c6c305f95333c8cf4c5283c2
leeyunbo <servers1@naver.com>
2021-06-02 23:03:05 +0900

    [개발] Email을 통한 유저 검색 메서드 개발

--- src/main/java/com/cloth/wardrobe/domain/member/MemberRepository.java
+++ src/main/java/com/cloth/wardrobe/domain/member/MemberRepository.java
@@ -4,8 +4,17 @@ import com.cloth.wardrobe.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    /**
+     * 소셜 로그인으로 반환되는 email을 통해 이미 생성된 사용자인지 판단하기 위한 메서드
+     * @param email
+     * @return Member 객체
+     */
+    Optional<Member> findByEmail(String email);
 
 }
