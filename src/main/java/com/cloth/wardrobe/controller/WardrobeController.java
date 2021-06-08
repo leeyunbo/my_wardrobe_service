ac9aeda7a6f13e863bd0556163439704e7d3b0df
leeyunbo <servers1@naver.com>
2021-06-08 22:01:35 +0900

    [개발] Wardrobe 관련 update, 조회 API 개발

--- src/main/java/com/cloth/wardrobe/controller/WardrobeController.java
+++ src/main/java/com/cloth/wardrobe/controller/WardrobeController.java
@@ -1,12 +1,14 @@
 package com.cloth.wardrobe.controller;
 
+import com.cloth.wardrobe.domain.clothes.WardrobeResponseRequestDto;
+import com.cloth.wardrobe.domain.clothes.WardrobeUpdateRequestDto;
 import com.cloth.wardrobe.dto.clothes.WardrobeSaveRequestDto;
 import com.cloth.wardrobe.service.WardrobeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
@@ -15,9 +17,19 @@ public class WardrobeController {
 
     private final WardrobeService wardrobeService;
 
-    @PostMapping("/api/v1/wardrobe/{memberId}")
+    @PostMapping("/api/v1/wardrobe")
     public Long save(@RequestBody WardrobeSaveRequestDto requestDto) {
         return wardrobeService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/wardrobe/{id}")
+    public Long update(@PathVariable Long id, @RequestBody WardrobeUpdateRequestDto wardrobeUpdateRequestDto) {
+        return wardrobeService.update(id, wardrobeUpdateRequestDto);
+    }
+
+    @GetMapping("/api/v1/wardrobe/{id}")
+    public WardrobeResponseRequestDto findById (@PathVariable Long id) {
+        return wardrobeService.findById(id);
     }
 
 
