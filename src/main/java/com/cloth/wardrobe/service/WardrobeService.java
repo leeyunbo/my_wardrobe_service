bcfa32099c06603ebb349da10f4c3dc2b45d764f
leeyunbo <servers1@naver.com>
2021-06-08 22:35:07 +0900

    [개발] WardrobeService 옷장 댓글 추가 기능 개발

--- src/main/java/com/cloth/wardrobe/service/WardrobeService.java
+++ src/main/java/com/cloth/wardrobe/service/WardrobeService.java
@@ -1,8 +1,9 @@
 package com.cloth.wardrobe.service;
 
 import com.cloth.wardrobe.domain.clothes.Wardrobe;
-import com.cloth.wardrobe.domain.clothes.WardrobeResponseRequestDto;
-import com.cloth.wardrobe.domain.clothes.WardrobeUpdateRequestDto;
+import com.cloth.wardrobe.dto.clothes.CommentSaveRequestDto;
+import com.cloth.wardrobe.dto.clothes.WardrobeResponseRequestDto;
+import com.cloth.wardrobe.dto.clothes.WardrobeUpdateRequestDto;
 import com.cloth.wardrobe.dto.clothes.WardrobeSaveRequestDto;
 import com.cloth.wardrobe.domain.clothes.WardrobeRepository;
 import lombok.RequiredArgsConstructor;
@@ -23,7 +24,8 @@ public class WardrobeService {
     @Transactional
     public Long update(Long id, WardrobeUpdateRequestDto requestDto) {
         Wardrobe wardrobe = wardrobeRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+                .orElseThrow(() ->
+                        new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         wardrobe.update(requestDto.getImage(), requestDto.getName(), requestDto.isPublic());
 
@@ -34,7 +36,8 @@ public class WardrobeService {
     @Transactional
     public WardrobeResponseRequestDto findById(Long id) {
         Wardrobe wardrobe = wardrobeRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+                .orElseThrow(() ->
+                        new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new WardrobeResponseRequestDto(wardrobe);
     }
@@ -48,4 +51,14 @@ public class WardrobeService {
         return id;
     }
 
+    @Transactional
+    public Long writeComment(Long id, CommentSaveRequestDto commentSaveRequestDto) {
+        Wardrobe wardrobe = wardrobeRepository.findById(id)
+                .orElseThrow(() ->
+                        new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        wardrobe.writeComment(commentSaveRequestDto.toEntity());
+
+        return id;
+    }
 }
