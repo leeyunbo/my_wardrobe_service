588cb3386664be7a13baaa4c91762fee842f3450
leeyunbo <servers1@naver.com>
2021-06-09 22:53:21 +0900

    [개발] WardrobeService 댓글 삭제 및 좋아요 감소 기능 개발

--- src/main/java/com/cloth/wardrobe/service/WardrobeService.java
+++ src/main/java/com/cloth/wardrobe/service/WardrobeService.java
@@ -1,10 +1,9 @@
 package com.cloth.wardrobe.service;
 
 import com.cloth.wardrobe.domain.clothes.Wardrobe;
-import com.cloth.wardrobe.dto.clothes.CommentSaveRequestDto;
-import com.cloth.wardrobe.dto.clothes.WardrobeResponseRequestDto;
-import com.cloth.wardrobe.dto.clothes.WardrobeUpdateRequestDto;
-import com.cloth.wardrobe.dto.clothes.WardrobeSaveRequestDto;
+import com.cloth.wardrobe.domain.community.Comment;
+import com.cloth.wardrobe.domain.community.CommentRepository;
+import com.cloth.wardrobe.dto.clothes.*;
 import com.cloth.wardrobe.domain.clothes.WardrobeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class WardrobeService {
 
     private final WardrobeRepository wardrobeRepository;
+    private final CommentRepository commentRepository;
 
     @Transactional
     public Long save(WardrobeSaveRequestDto requestDto) {
@@ -25,7 +25,7 @@ public class WardrobeService {
     public Long update(Long id, WardrobeUpdateRequestDto requestDto) {
         Wardrobe wardrobe = wardrobeRepository.findById(id)
                 .orElseThrow(() ->
-                        new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+                        new IllegalArgumentException("해당 옷장이 존재하지 않습니다. id=" + id));
 
         wardrobe.update(requestDto.getImage(), requestDto.getName(), requestDto.isPublic());
 
@@ -33,34 +33,88 @@ public class WardrobeService {
     }
 
 
+    /**
+     * 특정 옷장의 정보를 가져온다.
+     * @param id
+     * @return
+     */
     @Transactional
     public WardrobeResponseRequestDto findById(Long id) {
         Wardrobe wardrobe = wardrobeRepository.findById(id)
                 .orElseThrow(() ->
-                        new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+                        new IllegalArgumentException("해당 옷장이 존재하지 않습니다. id=" + id));
 
         return new WardrobeResponseRequestDto(wardrobe);
     }
 
+    /**
+     * isPublic이 true인 옷장들의 리스트들을 좋아요 순으로 가져온다. (페이징 사용)
+     */
+
+    /**
+     * isPublic이 true인 옷장들의 리스트들을 유저 이름 검색 기준으로 가져온다. (페이징 사용)
+     */
+
+
+    /**
+     * 좋아요 수를 증가시킨다.
+     */
     @Transactional
     public Long addLikeCnt(Long id) {
         Wardrobe wardrobe = wardrobeRepository.findById(id)
                 .orElseThrow(() ->
-                    new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+                        new IllegalArgumentException("해당 옷장이 존재하지 않습니다. id=" + id));
 
         wardrobe.addLikeCnt();
 
         return id;
     }
 
+    /**
+     * 좋아요 수를 감소시킨다.
+     */
+    @Transactional
+    public Long delLikeCnt(Long id) {
+        Wardrobe wardrobe = wardrobeRepository.findById(id)
+                .orElseThrow(() ->
+                        new IllegalArgumentException("해당 옷장이 존재하지 않습니다. id=" + id));
+
+        wardrobe.delLikeCnt();
+
+        return id;
+    }
+
+
+    /**
+     * 댓글을 작성한다.
+     */
     @Transactional
     public Long writeComment(Long id, CommentSaveRequestDto commentSaveRequestDto) {
         Wardrobe wardrobe = wardrobeRepository.findById(id)
                 .orElseThrow(() ->
-                        new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+                        new IllegalArgumentException("해당 옷장이 존재하지 않습니다. id=" + id));
 
         wardrobe.writeComment(commentSaveRequestDto.toEntity());
 
         return id;
     }
+
+    /**
+     * 댓글을 삭제한다.
+     */
+    @Transactional
+    public Long deleteComment(Long wardrobeId, Long commentId) {
+        Wardrobe wardrobe = wardrobeRepository.findById(wardrobeId)
+                .orElseThrow(() ->
+                        new IllegalArgumentException("해당 옷장이 존재하지 않습니다. id=" + wardrobeId));
+
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() ->
+                        new IllegalArgumentException("댓글이 존재하지 않습니다. id=" + commentId));
+
+        wardrobe.deleteComment(comment);
+
+        return wardrobeId;
+    }
+
 }
