4e0d971316789d7b56211518b31016a4df383296
leeyunbo <servers1@naver.com>
2021-06-13 20:25:19 +0900

    [개발] 옷장 페이징 기능 개발 완료

--- src/main/java/com/cloth/wardrobe/domain/clothes/WardrobeRepository.java
+++ src/main/java/com/cloth/wardrobe/domain/clothes/WardrobeRepository.java
@@ -1,14 +1,15 @@
 package com.cloth.wardrobe.domain.clothes;
 
-import com.cloth.wardrobe.domain.clothes.Wardrobe;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface WardrobeRepository extends JpaRepository<Wardrobe, Long> {
 
+    @Query("SELECT w FROM Wardrobe w WHERE w.isPublic = true ORDER BY w.likeCnt DESC")
     Page<Wardrobe> findAll(Pageable pageable);
 
 }
