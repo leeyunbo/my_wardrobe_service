7a7f8b3f2230fdf24ef21797cc008f9025a33379
leeyunbo <servers1@naver.com>
2021-06-19 00:27:58 +0900

    [개발] wardrobe list 화면 및 wardrobe 상세 화면 이동 로직 개발

--- src/main/java/com/cloth/wardrobe/controller/IndexController.java
+++ src/main/java/com/cloth/wardrobe/controller/IndexController.java
@@ -1,14 +1,22 @@
 package com.cloth.wardrobe.controller;
 
+import com.cloth.wardrobe.service.WardrobeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
 @Slf4j
 public class IndexController {
+
+    private final WardrobeService wardrobeService;
 
     @GetMapping("/")
     public String home() {
@@ -21,7 +29,15 @@ public class IndexController {
     }
 
     @GetMapping("/wardrobes")
-    public String wardrobes() {
+    public String wardrobes(Model model, @RequestParam(name = "page_number") int page) {
+        Pageable pageable = PageRequest.of(page-1,10);
+        model.addAttribute("wardrobes", wardrobeService.findAll(pageable));
         return "wardrobe/wardrobes-list";
+    }
+
+    @GetMapping("/wardrobes/{id}")
+    public String wardrobeById(Model model, @PathVariable(name = "id") Long id) {
+        model.addAttribute("wardrobe", wardrobeService.findById(id));
+        return "wardrobe/wardrobe-detail-view";
     }
 }
