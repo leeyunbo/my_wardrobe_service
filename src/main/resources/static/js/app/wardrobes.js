699cbe865be86a72fb251123ec6ad99600ac60b5
leeyunbo <servers1@naver.com>
2021-06-20 23:16:38 +0900

    [개발] 댓글 기능 개발

--- src/main/resources/static/js/app/wardrobes.js
+++ src/main/resources/static/js/app/wardrobes.js
@@ -4,6 +4,10 @@ var main = {
         $('#btn-save').on('click', function () {
             _this.save();
         });
+
+        $('#btn-comment-save').on('click', function () {
+            _this.save_comment();
+        })
     },
 
     save : function () {
@@ -26,9 +30,26 @@ var main = {
         }).fail(function (error) {
             alert(JSON.stringify(error));
         });
+    },
+
+    save_comment : function () {
+        var data = {
+            content : $('#comment-content').val()
+        };
+
+        $.ajax({
+            type: 'PUT',
+            url: '/api/v1/wardrobes/'+ $('#wardrobe_id').val() +'/comment',
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function() {
+            alert('댓글을 달았어요.');
+            window.location.href = '/wardrobes/' + $('#wardrobe_id').val();
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        })
     }
-
-
 };
 
 main.init();
