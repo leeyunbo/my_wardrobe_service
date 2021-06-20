454f7a4f8557c1cba78659ae3dff867a1c55e753
leeyunbo <servers1@naver.com>
2021-06-20 23:16:57 +0900

    [수정] 댓글 제목 필드 제거

--- src/main/java/com/cloth/wardrobe/dto/community/CommentResponseRequestDto.java
+++ src/main/java/com/cloth/wardrobe/dto/community/CommentResponseRequestDto.java
@@ -9,14 +9,12 @@ import lombok.Getter;
 public class CommentResponseRequestDto {
     private Long id;
     private Member member;
-    private String subject;
     private String content;
 
     @Builder
     public CommentResponseRequestDto(Comment comment) {
         this.id = comment.getId();
         this.member = comment.getMember();
-        this.subject = comment.getSubject();
         this.content = comment.getContent();
     }
 }
