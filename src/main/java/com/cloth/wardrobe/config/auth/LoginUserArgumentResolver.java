7b6a9cb53dd7c35b8d4db42c2808597eb28cc142
leeyunbo <servers1@naver.com>
2021-06-27 20:57:37 +0900

    [개발] 에노테이션으로 세션 정보 자동 주입될 수 있도록 에노테이션 개발

---
+++ src/main/java/com/cloth/wardrobe/config/auth/LoginUserArgumentResolver.java
@@ -0,0 +1,38 @@
+package com.cloth.wardrobe.config.auth;
+
+import com.cloth.wardrobe.config.auth.dto.SessionMember;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@Component
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final HttpSession httpSession;
+
+    /**
+     * 컨트롤러 메서드의 특정 파라미터를 지원하는지 판단하는 메서드
+     * @LoginUser 에노테이션이 붙어 있고, 파라미터의 클래스 타입이 SessionMember.class인 경우 true를 반환한다.
+     */
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean isLoginUserAnnotation = parameter.getParameterAnnotation(LoginUser.class) != null;
+        boolean isMemberClass = SessionMember.class.equals(parameter.getParameterType());
+        return isLoginUserAnnotation && isMemberClass;
+    }
+
+    /**
+     * 파라미터에 전달할 객체를 생성한다.
+     */
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        return httpSession.getAttribute("user");
+    }
+}
