3a14b8561b0fd65375a9da350a2505a51dd36704
leeyunbo <servers1@naver.com>
2021-07-11 23:14:36 +0900

    [개발] 옷 등록 관련 DTO 수정

--- src/main/java/com/cloth/wardrobe/dto/clothes/ClothSaveRequestDto.java
+++ src/main/java/com/cloth/wardrobe/dto/clothes/ClothSaveRequestDto.java
@@ -4,13 +4,15 @@ import com.cloth.wardrobe.domain.clothes.Cloth;
 import com.cloth.wardrobe.domain.clothes.Wardrobe;
 import com.cloth.wardrobe.domain.community.Post;
 import com.cloth.wardrobe.domain.member.Member;
+import com.cloth.wardrobe.domain.s3.Image;
 import com.cloth.wardrobe.dto.common.SaveRequestDto;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
@@ -18,7 +20,11 @@ public class ClothSaveRequestDto extends SaveRequestDto {
 
     @Setter
     private Member member;
+    @Setter
     private Wardrobe wardrobe;
+
+    private List<Image> images = new ArrayList<>();
+    private String clothName;
     private String clothType;
     private LocalDateTime buyingDate;
     private String buyingWay;
@@ -26,9 +32,11 @@ public class ClothSaveRequestDto extends SaveRequestDto {
     private String clothBrand;
 
     @Builder
-    public ClothSaveRequestDto(Member member, Wardrobe wardrobe, String clothType, LocalDateTime buyingDate, String buyingWay, String clothColor, String clothBrand) {
+    public ClothSaveRequestDto(Member member, Wardrobe wardrobe, List<Image> images, String clothName, String clothType, LocalDateTime buyingDate, String buyingWay, String clothColor, String clothBrand) {
         this.member = member;
         this.wardrobe = wardrobe;
+        this.images = images;
+        this.clothName = clothName;
         this.clothType = clothType;
         this.buyingDate = buyingDate;
         this.buyingWay = buyingWay;
@@ -39,11 +47,13 @@ public class ClothSaveRequestDto extends SaveRequestDto {
     @Override
     public Post toEntity() {
         return Cloth.builder()
+                .clothName(clothName)
                 .clothBrand(clothBrand)
                 .clothColor(clothColor)
                 .clothType(clothType)
                 .buyingWay(buyingWay)
                 .buyingDate(buyingDate)
+                .images(images)
                 .wardrobe(wardrobe)
                 .build();
     }
