e4134e86b4344b665b56cee8b0e2d72ae16011e4
Lee yun bok <44944031+leeyunbo@users.noreply.github.com>
2021-07-27 17:31:11 +0900

    [개발] MultiPartFile Path 관련 설정 추가

--- src/main/java/com/cloth/wardrobe/config/WebConfig.java
+++ src/main/java/com/cloth/wardrobe/config/WebConfig.java
@@ -2,8 +2,10 @@ package com.cloth.wardrobe.config;
 
 import com.cloth.wardrobe.config.auth.LoginUserArgumentResolver;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -14,8 +16,19 @@ public class WebConfig implements WebMvcConfigurer {
 
     private final LoginUserArgumentResolver loginUserArgumentResolver;
 
+    @Value("${resource.path}")
+    private String resourcePath;
+    @Value("${upload.path}")
+    private String uploadPath;
+
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(loginUserArgumentResolver);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(uploadPath)
+                .addResourceLocations(resourcePath);
     }
 }
