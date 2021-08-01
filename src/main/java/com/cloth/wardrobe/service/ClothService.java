681ccf61f9fcf7c7318109414f79ab8a1ce563aa
Lee yun bok <44944031+leeyunbo@users.noreply.github.com>
2021-08-01 15:05:51 +0900

    [수정] ClothService ResponseEntity를 이용하여 오류별 HTTP 상태코드 반환하도록 수정 개발

--- src/main/java/com/cloth/wardrobe/service/ClothService.java
+++ src/main/java/com/cloth/wardrobe/service/ClothService.java
@@ -11,6 +11,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 
@@ -30,39 +32,56 @@ public class ClothService {
     }
 
     @Transactional
-    public Page<Cloth> findAll(Pageable pageable) {
-        return clothRepository.findAll(pageable);
+    public ResponseEntity<?> findAll(Pageable pageable) {
+        try {
+            Page<Cloth> cloths = clothRepository.findAll(pageable);
+            return new ResponseEntity<>(cloths, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @Transactional
-    public Long addRecord(Long clothId, Member member, RecordSaveRequestDto recordSaveRequestDto) {
-        Cloth cloth = findClothById(clothId);
+    public ResponseEntity<?> addRecord(Long clothId, Member member, RecordSaveRequestDto recordSaveRequestDto) {
+        try {
+            Cloth cloth = findClothById(clothId);
 
-        if(!cloth.getMember().getEmail().equals(member.getEmail())) {
-            return -1L;
+            if (!cloth.getMember().getEmail().equals(member.getEmail())) {
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+
+            recordSaveRequestDto.setMember(member);
+            cloth.addRecord(recordSaveRequestDto.toEntity());
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
-        recordSaveRequestDto.setMember(member);
-        cloth.addRecord(recordSaveRequestDto.toEntity());
-
-        return clothId;
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @Transactional
-    public Long deleteRecord(Long clothId, Long recordId, Member member) {
-        Cloth cloth = findClothById(clothId);
+    public ResponseEntity<?> deleteRecord(Long clothId, Long recordId, Member member) {
+        try {
+            Cloth cloth = findClothById(clothId);
 
-        if(!cloth.getMember().getEmail().equals(member.getEmail())) {
-            return -1L;
+            if (!cloth.getMember().getEmail().equals(member.getEmail())) {
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+
+            Record record = recordRepository.findById(recordId)
+                    .orElseThrow(() ->
+                            new IllegalArgumentException("해당 기록이 존재하지 않습니다. id=" + recordId));
+
+            cloth.deleteRecord(record);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
-        Record record = recordRepository.findById(recordId)
-                .orElseThrow(() ->
-                        new IllegalArgumentException("해당 기록이 존재하지 않습니다. id=" + recordId));
-
-        cloth.deleteRecord(record);
-
-        return clothId;
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     private Cloth findClothById(Long clothId) {
