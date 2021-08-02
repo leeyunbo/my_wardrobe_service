7e0f8dcab67b957113c3be5f57e95a4be387d8c9
Lee yun bok <44944031+leeyunbo@users.noreply.github.com>
2021-08-02 23:37:33 +0900

    [개발] js 강제 수정시 발생하는 Exception 개발 (타 사용자의 자산에 강제로 접근하는 경우)

---
+++ src/main/java/com/cloth/wardrobe/exception/BadRequestException.java
@@ -0,0 +1,16 @@
+package com.cloth.wardrobe.exception;
+
+/**
+ * js 강제 수정하여 서버 로직에 접근하는 경우 발생하는 Exception
+ * 보통 특정 사용자가 js를 조작하여 타 사용자의 권한에 접근하려는 경우 발생한다.
+ */
+public class BadRequestException extends Exception {
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+
+    public BadRequestException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
