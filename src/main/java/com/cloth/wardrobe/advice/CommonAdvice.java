06ffdd4f15064c220f8fe39d782690d186e60225
Lee yun bok <44944031+leeyunbo@users.noreply.github.com>
2021-08-22 01:18:05 +0900

    [수정] Exception, InvalidArgumentException 제거
    공통적으로 발생할 수 있는 Exception은 Advice에서 제거
    -> 컨트롤러마다 로직 따로 개발

--- src/main/java/com/cloth/wardrobe/advice/CommonAdvice.java
+++ src/main/java/com/cloth/wardrobe/advice/CommonAdvice.java
@@ -1,7 +1,8 @@
 package com.cloth.wardrobe.advice;
 
-import com.cloth.wardrobe.dto.common.ResponseForError;
+import com.cloth.wardrobe.dto.common.Response;
 import com.cloth.wardrobe.exception.BadRequestException;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
@@ -15,13 +16,14 @@ import org.springframework.web.method.annotation.MethodArgumentTypeMismatchExcep
  * 모든 데이터에 대한 공통 Exception을 관리하는 Advice
  * 2021-08-16 이윤복
  */
+@Log4j2
 @RestControllerAdvice
 @Order(Ordered.HIGHEST_PRECEDENCE)   // Advice의 우선순위를 의미한다.
 public class CommonAdvice {
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity<?> methodArgumentTypeMismatchError(Exception e) {
-        ResponseForError responseForError = new ResponseForError();
+        Response responseForError = new Response();
         responseForError.set_code(200);
         responseForError.set_message("잘못된 입력입니다.");
         return new ResponseEntity<>(responseForError, HttpStatus.BAD_REQUEST);
@@ -29,25 +31,9 @@ public class CommonAdvice {
 
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<?> badRequest(Exception e) {
-        ResponseForError responseForError = new ResponseForError();
+        Response responseForError = new Response();
         responseForError.set_code(200);
         responseForError.set_message("잘못된 접근입니다.");
         return new ResponseEntity<>(responseForError, HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<?> noContent(Exception e) {
-        ResponseForError responseForError = new ResponseForError();
-        responseForError.set_code(200);
-        responseForError.set_message("잘못된 접근입니다.");
-        return new ResponseEntity<>(responseForError, HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<?> internalServerError(Exception e) {
-        ResponseForError responseForError = new ResponseForError();
-        responseForError.set_code(200);
-        responseForError.set_message("서버 점검 중입니다.");
-        return new ResponseEntity<>(responseForError, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
--- src/main/java/com/cloth/wardrobe/advice/PostAdvice.java
+++ src/main/java/com/cloth/wardrobe/advice/PostAdvice.java
@@ -1,7 +1,8 @@
 package com.cloth.wardrobe.advice;
 
-import com.cloth.wardrobe.dto.common.ResponseForError;
+import com.cloth.wardrobe.dto.common.Response;
 import com.cloth.wardrobe.exception.WrongAccessException;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
@@ -14,13 +15,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
  * 게시판과 같이 등록할 수 있는 데이터에 대한 Exception을 관리하는 Advice
  * 2021-08-02 이윤복
  */
+@Log4j2
 @RestControllerAdvice
 @Order(Ordered.HIGHEST_PRECEDENCE)   // Advice의 우선순위를 의미한다.
 public class PostAdvice {
 
     @ExceptionHandler(WrongAccessException.class)
     public ResponseEntity<?> doNotClicked(Exception e) {
-        ResponseForError responseForError = new ResponseForError();
+        Response responseForError = new Response();
         responseForError.set_code(200);
         responseForError.set_message("권한이 없습니다.");
         return new ResponseEntity<>(responseForError, HttpStatus.BAD_REQUEST);
