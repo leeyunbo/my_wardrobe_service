d34a664f4ee9f2b6ac44452b4eba34403c905363
Lee yun bok <44944031+leeyunbo@users.noreply.github.com>
2021-08-22 01:22:02 +0900

    [수정] WardrobeService 리팩토링
    1. 모든 메서드에서 ResponseEntity에 Response 객체 담아서 리턴하도록 수정
    2. 쓸데 없는 메서드 분리 제거 (findXXXByXXX와 같은 쓸데 없는 로직 분리)

--- src/main/java/com/cloth/wardrobe/service/WardrobeService.java
+++ src/main/java/com/cloth/wardrobe/service/WardrobeService.java
@@ -5,6 +5,7 @@ import com.cloth.wardrobe.domain.clothes.Wardrobe;
 import com.cloth.wardrobe.domain.community.Comment;
 import com.cloth.wardrobe.domain.s3.Image;
 import com.cloth.wardrobe.dto.clothes.element.ContentForWardrobes;
+import com.cloth.wardrobe.dto.common.Response;
 import com.cloth.wardrobe.exception.BadRequestException;
 import com.cloth.wardrobe.repository.ClothRepository;
 import com.cloth.wardrobe.repository.CommentRepository;
@@ -12,8 +13,8 @@ import com.cloth.wardrobe.domain.member.Member;
 import com.cloth.wardrobe.domain.member.MemberRepository;
 import com.cloth.wardrobe.dto.clothes.*;
 import com.cloth.wardrobe.repository.WardrobeRepository;
-import com.cloth.wardrobe.dto.community.CommentResponseRequestDto;
-import com.cloth.wardrobe.dto.community.CommentSaveRequestDto;
+import com.cloth.wardrobe.dto.community.ResponseForComment;
+import com.cloth.wardrobe.dto.community.RequestForCommentSave;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -59,7 +60,9 @@ public class WardrobeService {
      */
     @Transactional
     public ResponseEntity<?> update(Long wardrobeId, RequestForWardrobeUpdate requestDto) {
-        Wardrobe wardrobe = findWardrobeById(wardrobeId);
+        Wardrobe wardrobe = wardrobeRepository.findById(wardrobeId)
+                .orElseThrow(() ->
+                        new BadRequestException("해당 옷장이 존재하지 않습니다. id=" + wardrobeId));
         wardrobe.update(requestDto.getImage(), requestDto.getName(), requestDto.getIsPublic());
         return new ResponseEntity<>(HttpStatus.OK);
     }
@@ -70,7 +73,9 @@ public class WardrobeService {
      */
     @Transactional
     public ResponseEntity<?> findById(Long wardrobeId) {
-        Wardrobe wardrobe = findWardrobeById(wardrobeId);
+        Wardrobe wardrobe = wardrobeRepository.findById(wardrobeId)
+                .orElseThrow(() ->
+                        new BadRequestException("해당 옷장이 존재하지 않습니다. id=" + wardrobeId));
         return new ResponseEntity<>(new ResponseForWardrobe(wardrobe), HttpStatus.OK);
     }
 
@@ -95,10 +100,18 @@ public class WardrobeService {
         ResponseForWardrobes responseForWardrobes = new ResponseForWardrobes();
 
         for(Wardrobe wardrobe : wardrobeRepository.findAll()) {
-            wardrobes.add(new ContentForWardrobes(wardrobe.getId(), wardrobe.getName(), wardrobe.getClothes().size(), wardrobe.getLikeCnt(), wardrobe.getMember().getName()));
+            wardrobes.add(new ContentForWardrobes(
+                    wardrobe.getId(),
+                    wardrobe.getName(),
+                    wardrobe.getClothes().size(),
+                    wardrobe.getLikeCnt(),
+                    wardrobe.getMember().getName())
+            );
         }
 
         responseForWardrobes.setContents(wardrobes);
+        responseForWardrobes.set_code(200);
+        responseForWardrobes.set_message("OK");
 
         return new ResponseEntity<>(responseForWardrobes, HttpStatus.OK);
     }
@@ -107,11 +120,12 @@ public class WardrobeService {
      * 댓글을 모두 가져온다.
      */
     @Transactional
-    public List<CommentResponseRequestDto> getComments(Long wardrobeId) {
-        Wardrobe wardrobe = findWardrobeById(wardrobeId);
+    public List<ResponseForComment> getComments(Long wardrobeId) {
+        Wardrobe wardrobe = wardrobeRepository.findById(wardrobeId).orElseThrow(() -> new IllegalArgumentException("해당 옷장이 존재하지 않습니다. id=" + wardrobeId));
 
-        return wardrobe.getComments().stream()
-                .map(CommentResponseRequestDto::new)
+        return wardrobe.getComments()
+                .stream()
+                .map(ResponseForComment::new)
                 .collect(Collectors.toList());
     }
 
@@ -120,14 +134,18 @@ public class WardrobeService {
      * 댓글을 작성한다.
      */
     @Transactional
-    public ResponseEntity<?> writeComment(Long wardrobeId, Long memberId, CommentSaveRequestDto commentSaveRequestDto) {
-        Wardrobe wardrobe = findWardrobeById(wardrobeId);
-        Member member = findMemberById(memberId);
+    public ResponseEntity<?> writeComment(Long wardrobeId, Long memberId, RequestForCommentSave commentSaveRequestDto) {
+        Wardrobe wardrobe =  wardrobeRepository.findById(wardrobeId).orElseThrow(() -> new BadRequestException("해당 옷장이 존재하지 않습니다. id=" + wardrobeId));
+        Member member = memberRepository.findById(memberId).orElseThrow(() -> new BadRequestException("해당 멤버가 존재하지 않습니다. id=" + memberId));
 
         commentSaveRequestDto.setMember(member);
         wardrobe.writeComment(commentSaveRequestDto.toEntity());
 
-        return new ResponseEntity<>(HttpStatus.OK);
+        Response response = new Response();
+        response.set_code(200);
+        response.set_message("OK");
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     /**
@@ -135,15 +153,18 @@ public class WardrobeService {
      */
     @Transactional
     public ResponseEntity<?> deleteComment(Long wardrobeId, Long commentId, Member member) {
-        Wardrobe wardrobe = findWardrobeById(wardrobeId);
-        Comment comment = findCommentById(commentId);
+        Wardrobe wardrobe = wardrobeRepository.findById(wardrobeId).orElseThrow(() -> new BadRequestException("해당 옷장이 존재하지 않습니다. id=" + wardrobeId));
+        Comment comment = commentRepository.findById(commentId).orElseThrow(() -> new BadRequestException("댓글이 존재하지 않습니다. id=" + commentId));
 
-        if (!comment.getMember().getEmail().equals(member.getEmail()))
-            throw new BadRequestException("올바르지 않은 접근입니다.");
+        if (!comment.getMember().getEmail().equals(member.getEmail())) throw new BadRequestException("올바르지 않은 접근입니다.");
 
         wardrobe.deleteComment(comment);
 
-        return new ResponseEntity<>(HttpStatus.OK);
+        Response response = new Response();
+        response.set_code(200);
+        response.set_message("OK");
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     /**
@@ -152,11 +173,10 @@ public class WardrobeService {
     @Transactional
     public ResponseEntity<?> addCloth(Long wardrobeId, RequestForClothSave requestForClothSave, Member member, MultipartFile file) {
         try {
-            Wardrobe wardrobe = findWardrobeById(wardrobeId);
+            Wardrobe wardrobe = wardrobeRepository.findById(wardrobeId).orElseThrow(() -> new BadRequestException("해당 옷장이 존재하지 않습니다. id=" + wardrobeId));
             Image image = new Image().fileUpload(file, member.getEmail());
 
-            if (!wardrobe.getMember().getEmail().equals(member.getEmail()))
-                throw new BadRequestException("올바르지 않은 접근입니다.");
+            if (!wardrobe.getMember().getEmail().equals(member.getEmail())) throw new BadRequestException("올바르지 않은 접근입니다.");
 
             requestForClothSave.setMember(member);
             requestForClothSave.setImage(image);
@@ -166,7 +186,11 @@ public class WardrobeService {
             throw new BadRequestException("파일이 손상되었습니다.");
         }
 
-        return new ResponseEntity<>(HttpStatus.OK);
+        Response response = new Response();
+        response.set_code(200);
+        response.set_message("OK");
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     /**
@@ -174,38 +198,17 @@ public class WardrobeService {
      */
     @Transactional
     public ResponseEntity<?> deleteCloth(Long wardrobeId, Long clothId, Member member) {
-        Wardrobe wardrobe = findWardrobeById(wardrobeId);
-        Cloth cloth = findClothById(clothId);
+        Wardrobe wardrobe = wardrobeRepository.findById(wardrobeId).orElseThrow(() -> new BadRequestException("해당 옷장이 존재하지 않습니다. id=" + wardrobeId));
+        Cloth cloth = clothRepository.findById(clothId).orElseThrow(() -> new BadRequestException("해당 품목이 존재하지 않습니다. id=" + clothId));
 
-        if(!wardrobe.getMember().getEmail().equals(member.getEmail()))
-            throw new BadRequestException("올바르지 않은 접근입니다.");
+        if(!wardrobe.getMember().getEmail().equals(member.getEmail())) throw new BadRequestException("올바르지 않은 접근입니다.");
 
         wardrobe.deleteCloth(cloth);
 
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
+        Response response = new Response();
+        response.set_code(200);
+        response.set_message("OK");
 
-    private Member findMemberById(Long memberId) {
-        return memberRepository.findById(memberId)
-                .orElseThrow(() ->
-                        new IllegalArgumentException("해당 멤버가 존재하지 않습니다. id=" + memberId));
-    }
-
-    private Wardrobe findWardrobeById(Long wardrobeId) {
-        return wardrobeRepository.findById(wardrobeId)
-                .orElseThrow(() ->
-                        new IllegalArgumentException("해당 옷장이 존재하지 않습니다. id=" + wardrobeId));
-    }
-
-    private Comment findCommentById(Long commentId) {
-        return commentRepository.findById(commentId)
-                .orElseThrow(() ->
-                        new IllegalArgumentException("댓글이 존재하지 않습니다. id=" + commentId));
-    }
-
-    private Cloth findClothById(Long clothId) {
-        return clothRepository.findById(clothId)
-                .orElseThrow(() ->
-                        new IllegalArgumentException("해당 품목이 존재하지 않습니다. id=" + clothId));
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
