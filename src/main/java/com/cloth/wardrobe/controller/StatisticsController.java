40751eba6c4103da29171580abf3fc8e89a7dfc6
Lee yun bok <44944031+leeyunbo@users.noreply.github.com>
2021-08-27 11:14:37 +0900

    [개발] 통계 관련 컨트롤러 개발
    통계 관련 컨트롤러 개발
    - getStatistics() : 통계 정보를 반환해주는 api 메서드

---
+++ src/main/java/com/cloth/wardrobe/controller/StatisticsController.java
@@ -0,0 +1,22 @@
+package com.cloth.wardrobe.controller;
+
+import com.cloth.wardrobe.service.StatisticsService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class StatisticsController {
+
+    private final StatisticsService statisticsService;
+
+    @GetMapping("api/v1/statistics")
+    public ResponseEntity<?> getStatistics() {
+        return statisticsService.getStatistics();
+    }
+
+}
