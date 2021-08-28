b8b9b93f6f8b61cdcd7639497e5dd69ef3dc0cb3
Lee yun bok <44944031+leeyunbo@users.noreply.github.com>
2021-08-29 00:17:26 +0900

    [수정] Response @Getter 추가
    @Getter를 추가 안해서, Json Convert 할 수 없음 오류 발생.
    포함하여 수정

--- src/main/java/com/cloth/wardrobe/dto/common/Response.java
+++ src/main/java/com/cloth/wardrobe/dto/common/Response.java
@@ -1,8 +1,10 @@
 package com.cloth.wardrobe.dto.common;
 
-import lombok.Data;
+import lombok.Getter;
 import lombok.Setter;
 
+@Getter
+@Setter
 public class Response {
     @Setter private Integer _code;
     @Setter private String _message;
