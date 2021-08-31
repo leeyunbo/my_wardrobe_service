3a784b216e133554b0bae45ad8e3480562a8efcb
Lee yun bok <44944031+leeyunbo@users.noreply.github.com>
2021-08-31 17:19:40 +0900

    [업데이트] 코디도 이미지 포함하여 등록될 수 있도록 개발
    이미지 파일 포함하여 저장될 수 있도록 비즈니스 로직 개발
    -> IOException catch문 추가
    -> Image 객체 담을 수 있도록 로직 수정

--- src/main/java/com/cloth/wardrobe/service/ClothService.java
+++ src/main/java/com/cloth/wardrobe/service/ClothService.java
@@ -3,6 +3,7 @@ package com.cloth.wardrobe.service;
 import com.cloth.wardrobe.domain.clothes.Cloth;
 import com.cloth.wardrobe.domain.clothes.Record;
 import com.cloth.wardrobe.domain.member.Member;
+import com.cloth.wardrobe.domain.s3.Image;
 import com.cloth.wardrobe.dto.clothes.ResponseForCloth;
 import com.cloth.wardrobe.dto.clothes.ResponseForClothes;
 import com.cloth.wardrobe.dto.clothes.element.ContentForCloth;
@@ -16,7 +17,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
 import javax.transaction.Transactional;
+import java.awt.image.MultiPixelPackedSampleModel;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,7 +35,9 @@ public class ClothService {
 
     @Transactional
     public ResponseEntity<?> findById(Long clothId) {
-        Cloth cloth = clothRepository.findById(clothId).orElseThrow(() -> new IllegalArgumentException("해당 품목이 존재하지 않습니다. id=" + clothId));
+        Cloth cloth = clothRepository.findById(clothId)
+                .orElseThrow(() -> new BadRequestException("해당 품목이 존재하지 않습니다. id=" + clothId));
+
         ResponseForCloth responseForCloth = new ResponseForCloth(cloth);
         responseForCloth.set_code(200);
         responseForCloth.set_message("OK");
@@ -55,41 +62,51 @@ public class ClothService {
     }
 
     @Transactional
-    public ResponseEntity<?> addRecord(Long clothId, Member member, RequestForRecordSave recordSaveRequestDto) {
+    public ResponseEntity<?> addRecord(Long clothId, Member member, RequestForRecordSave requestForRecordSave, MultipartFile file) {
         Cloth cloth = clothRepository.findById(clothId)
-                .orElseThrow(() ->
-                        new IllegalArgumentException("해당 품목이 존재하지 않습니다. id=" + clothId));
+                .orElseThrow(() -> new BadRequestException("해당 품목이 존재하지 않습니다. id=" + clothId));
 
-        if (!cloth.getMember().getEmail().equals(member.getEmail())) throw new BadRequestException("올바르지 않은 접근입니다.");
+        if (cloth.getMember().getEmail().equals(member.getEmail())) {
+            try {
+                Image image = new Image().fileUpload(file, member.getEmail());
+                requestForRecordSave.setImage(image);
+                requestForRecordSave.setMember(member);
+                cloth.addRecord(requestForRecordSave.toEntity());
 
-        recordSaveRequestDto.setMember(member);
-        cloth.addRecord(recordSaveRequestDto.toEntity());
+                Response response = new Response();
+                response.set_code(200);
+                response.set_message("OK");
 
-        Response response = new Response();
-        response.set_code(200);
-        response.set_message("OK");
-
-        return new ResponseEntity<>(response, HttpStatus.OK);
+                return new ResponseEntity<>(response, HttpStatus.OK);
+            } catch (IOException e) {
+                throw new BadRequestException("올바르지 않은 요청입니다.");
+            }
+        }
+        else {
+            throw new BadRequestException("올바르지 않은 접근입니다.");
+        }
     }
 
     @Transactional
     public ResponseEntity<?> deleteRecord(Long clothId, Long recordId, Member member) {
         Cloth cloth = clothRepository.findById(clothId)
-                .orElseThrow(() ->
-                        new IllegalArgumentException("해당 품목이 존재하지 않습니다. id=" + clothId));
+                .orElseThrow(() -> new BadRequestException("해당 품목이 존재하지 않습니다."));
 
-        if (!cloth.getMember().getEmail().equals(member.getEmail())) throw new BadRequestException("올바르지 않은 접근입니다.");
+        if (cloth.getMember().getEmail().equals(member.getEmail())) {
 
-        Record record = recordRepository.findById(recordId)
-                    .orElseThrow(() ->
-                            new IllegalArgumentException("해당 기록이 존재하지 않습니다. id=" + recordId));
+            Record record = recordRepository.findById(recordId)
+                    .orElseThrow(() -> new BadRequestException("해당 기록이 존재하지 않습니다."));
 
-        cloth.deleteRecord(record);
+            cloth.deleteRecord(record);
 
-        Response response = new Response();
-        response.set_code(200);
-        response.set_message("OK");
+            Response response = new Response();
+            response.set_code(200);
+            response.set_message("OK");
 
-        return new ResponseEntity<>(response, HttpStatus.OK);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        }
+        else {
+            throw new BadRequestException("올바르지 않은 접근입니다.");
+        }
     }
 }
