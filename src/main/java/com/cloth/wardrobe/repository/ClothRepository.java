74b0b55ef2f058db4e769ad9a18315d10c62c503
Lee yun bok <44944031+leeyunbo@users.noreply.github.com>
2021-09-02 23:45:58 +0900

    [개발] 특정 유저가 등록한 옷 가져오는 메서드 추가 개발
    특정 유저가 등록한 옷 가져오는 메서드 추가 개발
    -> 나만의 선호도 보기에서 활용

--- src/main/java/com/cloth/wardrobe/repository/ClothRepository.java
+++ src/main/java/com/cloth/wardrobe/repository/ClothRepository.java
@@ -2,6 +2,7 @@ package com.cloth.wardrobe.repository;
 
 import com.cloth.wardrobe.domain.clothes.Cloth;
 import com.cloth.wardrobe.domain.clothes.Wardrobe;
+import com.cloth.wardrobe.domain.member.Member;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -15,4 +16,6 @@ public interface ClothRepository extends JpaRepository<Cloth, Long> {
 
     @Query("SELECT c FROM Cloth c ORDER BY c.likeCnt DESC")
     List<Cloth> findAll();
+
+    List<Cloth> findClothsByMemberOrderByLikeCntDesc(Member member);
 }
