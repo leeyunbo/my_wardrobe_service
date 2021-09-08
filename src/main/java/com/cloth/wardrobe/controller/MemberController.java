7a6bc7aff61fa905780fa0fafb58c5b3daf2a52a
Lee yun bok <44944031+leeyunbo@users.noreply.github.com>
2021-09-08 23:15:15 +0900

    [개발] 사용자 옷장 정보 가져오기 API 개발
    사용자 옷장 정보 가져오기 API 개발
    1. session을 통해 사용자 정보를 가져온다.
    2. 사용자 정보를 통해 옷장 상세 정보를 가져온다.

---
+++ src/main/java/com/cloth/wardrobe/controller/MemberController.java
@@ -0,0 +1,27 @@
+package com.cloth.wardrobe.controller;
+
+import com.cloth.wardrobe.config.auth.CustomOAuth2MemberService;
+import com.cloth.wardrobe.config.auth.LoginUser;
+import com.cloth.wardrobe.config.auth.dto.SessionMember;
+import com.cloth.wardrobe.domain.member.Member;
+import com.cloth.wardrobe.dto.clothes.ResponseForWardrobe;
+import com.cloth.wardrobe.service.WardrobeService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api/v1/member")
+@RequiredArgsConstructor
+@Slf4j
+public class MemberController {
+    private final WardrobeService wardrobeService;
+    private final CustomOAuth2MemberService customOAuth2MemberService;
+
+    @GetMapping("/wardrobe")
+    public ResponseEntity<ResponseForWardrobe> findWardrobeByMember(@LoginUser SessionMember sessionMember) {
+        Member member = customOAuth2MemberService.getMemberBySession(sessionMember);
+        return wardrobeService.findByMember(member);
+    }
+}
---
+++ src/main/resources/templates/common.js
@@ -0,0 +1,11 @@
+var main = {
+    init : function () {
+
+    },
+
+    createTable : function () {
+
+    }
+
+
+}
