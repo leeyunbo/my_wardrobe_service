309d55e3a4bf269e9e9d3f5237c50cdb470785aa
Lee yun bok <44944031+leeyunbo@users.noreply.github.com>
2021-09-09 21:23:48 +0900

    [스타일 변경]

--- src/main/java/com/cloth/wardrobe/repository/ImageRepository.java
+++ src/main/java/com/cloth/wardrobe/repository/ImageRepository.java
@@ -1,6 +1,6 @@
 package com.cloth.wardrobe.repository;
 
-import com.cloth.wardrobe.domain.s3.Image;
+import com.cloth.wardrobe.domain.common.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ImageRepository extends JpaRepository<Image, Long> {
