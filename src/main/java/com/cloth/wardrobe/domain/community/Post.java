74c25869ed4fcc41cce31bf6bfc1d09cb6c73bbc
Lee yun bok <44944031+leeyunbo@users.noreply.github.com>
2021-09-09 21:21:14 +0900

    [수정] 최상위 클래스 Post에 공통 메서드 추가
    최상위 클래스 Post에 공통 메서드 추가
    1. writeComment() : 댓글 작성
    2. deleteComment() : 댓글 제거

--- src/main/java/com/cloth/wardrobe/domain/community/Post.java
+++ src/main/java/com/cloth/wardrobe/domain/community/Post.java
@@ -2,10 +2,13 @@ package com.cloth.wardrobe.domain.community;
 
 
 import com.cloth.wardrobe.domain.BaseTimeEntity;
+import com.cloth.wardrobe.domain.clothes.Cloth;
 import com.cloth.wardrobe.domain.clothes.MethodType;
+import com.cloth.wardrobe.domain.clothes.Record;
 import lombok.Getter;
 import lombok.Setter;
 import javax.persistence.MappedSuperclass;
+import java.util.List;
 
 
 @Setter
@@ -14,6 +17,6 @@ import javax.persistence.MappedSuperclass;
 public abstract class Post extends BaseTimeEntity {
 
     public abstract Post changeLikeCnt(Like like, MethodType type);
-
-
+    public abstract Post writeComment(Comment comment);
+    public abstract Post deleteComment(Comment comment);
 }
