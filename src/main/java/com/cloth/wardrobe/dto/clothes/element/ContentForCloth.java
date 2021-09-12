e128512e6bb75cb36766b06dd19a9c83678966ca
Lee yun bok <44944031+leeyunbo@users.noreply.github.com>
2021-09-12 23:10:56 +0900

    [긴급 오류 수정] ContentForCloth에 Image 클래스 포함되어 무한 사이클 현상 수정

--- src/main/java/com/cloth/wardrobe/dto/clothes/element/ContentForCloth.java
+++ src/main/java/com/cloth/wardrobe/dto/clothes/element/ContentForCloth.java
@@ -1,14 +1,23 @@
 package com.cloth.wardrobe.dto.clothes.element;
 
+import com.cloth.wardrobe.domain.clothes.Cloth;
 import com.cloth.wardrobe.domain.common.Image;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor
+@NoArgsConstructor
 public class ContentForCloth {
     private Long id;
-    private Image image;
+    private String imageServerPath;
     private String buyingDate;
     private String clothBrand;
+
+    public ContentForCloth(Cloth cloth) {
+        this.id = cloth.getId();
+        this.imageServerPath = cloth.getImage().getImageServerPath();
+        this.buyingDate = cloth.getBuyingDate();
+        this.clothBrand = cloth.getClothBrand();
+    }
 }
