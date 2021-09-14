15543f89835a386bc187caf387713557f6b0fbe7
Lee yun bok <44944031+leeyunbo@users.noreply.github.com>
2021-09-14 23:38:59 +0900

    [리팩토링] CheckService 내에 체크 로직 분리
    로직 실행 전 수행하는 체크 로직 CheckService로 분리

---
+++ src/main/java/com/cloth/wardrobe/service/CheckService.java
@@ -0,0 +1,14 @@
+package com.cloth.wardrobe.service;
+
+
+import com.cloth.wardrobe.exception.WrongAccessException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CheckService {
+    protected void confirmRightApproach(String emailForSession, String email) {
+        if (emailForSession.equals(email)) {
+            throw new WrongAccessException("올바르지 않은 접근입니다.");
+        }
+    }
+}
