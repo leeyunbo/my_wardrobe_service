603cec8912ef9d35468c6b54074f04691b94f940
Lee yun bok <44944031+leeyunbo@users.noreply.github.com>
2021-09-23 00:35:04 +0900

    [개발] api 요청 로그 필터 개발
    /api/v1/** 에 대한 요청 로그 남기도록 필터 개발
    
    [UUID][Method : request url] 형식으로 로그 출력

--- src/main/java/com/cloth/wardrobe/config/WebConfig.java
+++ src/main/java/com/cloth/wardrobe/config/WebConfig.java
@@ -1,13 +1,17 @@
 package com.cloth.wardrobe.config;
 
 import com.cloth.wardrobe.config.auth.LoginUserArgumentResolver;
+import com.cloth.wardrobe.filter.LogFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.servlet.Filter;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -30,5 +34,18 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler(uploadPath)
                 .addResourceLocations(resourcePath);
+    }
+
+    /**
+     * 로그 필터 등록
+     */
+    @Bean
+    public FilterRegistrationBean<Filter> logFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LogFilter()); // 어떤 필터?
+        filterRegistrationBean.setOrder(1); // 어떤 순서?
+        filterRegistrationBean.addUrlPatterns("/api/v1/*"); // 어떤 url pattern?
+
+        return filterRegistrationBean;
     }
 }
