afb45bd5e8e18ae588b3f43079e4a396a3654e33
Lee yun bok <44944031+leeyunbo@users.noreply.github.com>
2021-09-27 23:13:05 +0900

    [코드 스타일 변경] ApiAuthFilter -> ApiAuthorizationFilter로 명칭 변경

--- src/main/java/com/cloth/wardrobe/filter/ApiAuthorizationFilter.java
+++ src/main/java/com/cloth/wardrobe/filter/ApiAuthorizationFilter.java
@@ -5,17 +5,17 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
-public class ApiAuthFilter implements Filter {
+public class ApiAuthorizationFilter implements Filter {
 
-    private String principalRequestHeader = "Authorization";
-    private String principalRequestValue = "fdasrv34atdzb4zeex7y";
+    public static final String API_KEY = "fdasrv34atdzb4zeex7y";
+    public static final String API_KEY_HEADER = "Authorization";
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) request;
 
         try {
-            if (!httpServletRequest.getHeader(principalRequestHeader).equals(principalRequestValue)) {
+            if (!httpServletRequest.getHeader(API_KEY_HEADER).equals(API_KEY)) {
                 throw new ApiKeyInvalidException("api key is invalid");
             }
         } catch (NullPointerException e) {
