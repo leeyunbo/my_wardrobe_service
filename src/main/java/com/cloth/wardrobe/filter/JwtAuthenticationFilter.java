d1100a4060c8e3240c44cd30e4dc4a30957b2ca8
Lee yun bok <44944031+leeyunbo@users.noreply.github.com>
2021-10-10 01:21:36 +0900

    [개발] Jwt Authentication 로직 변경
    1. Google Validate 메서드 활용하여 개발
    2. 검증하여 맞으면 request에 member 정보 추가
    3. 아니면 Exception 발생

--- src/main/java/com/cloth/wardrobe/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/cloth/wardrobe/filter/JwtAuthenticationFilter.java
@@ -1,10 +1,12 @@
 package com.cloth.wardrobe.filter;
 
+import com.cloth.wardrobe.config.auth.dto.RequestForMember;
 import com.cloth.wardrobe.dto.common.ResponseMessage;
 import com.cloth.wardrobe.entity.member.Role;
+import com.cloth.wardrobe.exception.GoogleIdAuthenticationException;
 import com.cloth.wardrobe.exception.JwtAuthenticaitonException;
 import com.cloth.wardrobe.properties.AuthorizationProperties;
-import com.cloth.wardrobe.service.JwtService;
+import com.cloth.wardrobe.service.MemberService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
@@ -26,66 +28,51 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Slf4j
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-
-    @Resource
-    private Environment env;
-
     @Autowired
-    private JwtService jwtService;
+    private MemberService memberService;
 
     @Autowired
     private AuthorizationProperties authorizationProperties;
 
     private RequestMatcher requestMatcher = new AntPathRequestMatcher("/api/**");
 
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        // permitAll이라고 해서 필터가 아예 적용되지 않도록 하는 것이 아니다. 필터는 항상 통과한다.
-        //
         if (requestMatcher.matches(request)) {
 
             String jwt = request.getHeader(authorizationProperties.getAuth_header()).replace("Bearer ", "");
 
             if (jwt.isEmpty()) {
-                throw new JwtAuthenticaitonException(ResponseMessage.INVALID_AUTHENTICATION);
+                throw new JwtAuthenticaitonException(ResponseMessage.DO_NOT_FOUND_JWT_TOKEN);
             } else {
+                try {
+                    RequestForMember requestForMember = memberService.validateToken(request);
 
-                // JWT가 있다면 유효한지 검사
-                if (!jwtService.isValidateToken(jwt)) {
-                    throw new JwtAuthenticaitonException(ResponseMessage.INVALID_AUTHENTICATION);
+//                    Map<String, Object> attributes = new HashMap<>();
+//
+//                    String userNameAttributeName = "sub";
+//
+//                    List<GrantedAuthority> authorities = new ArrayList<>();
+//                    authorities.add(new SimpleGrantedAuthority(Role.USER.getKey()));
+//
+//                    OAuth2User userDetails = new DefaultOAuth2User(authorities, attributes, userNameAttributeName);
+//                    OAuth2AuthenticationToken authentication = new OAuth2AuthenticationToken(userDetails, authorities, userNameAttributeName);
+//
+//                    authentication.setDetails(userDetails);
+//                    SecurityContextHolder.getContext().setAuthentication(authentication);
+
+                    request.setAttribute("member", requestForMember);
+                } catch (GoogleIdAuthenticationException e) {
+                    throw e;
                 }
-
-                // JWT가 만료되었는지 확인
-                if (jwtService.isTokenExpired(jwt)) {
-                    throw new JwtAuthenticaitonException(ResponseMessage.EXPIRED_AUTHENTICATION);
-                }
-
-                Map<String, Object> attributes = jwtService.getBodyFromToken(jwt);
-                log.info("jwtAuthenticationFilter.doFilterInternal() : [{}]", attributes.toString());
-
-                // JWT로부터 사용자 정보를 추출하여 인증 정보를 만든 후 SecurityContext에 넣는다.
-                // 결과적으로 처음 인증 공급자로부터 받은 정보를 JWT에 넣었고 헤더 통해 다시 받으면
-                // 그것을 OAuth2User로 다시 복원해서 시큐리티의 인증정보에 넣어야 시큐리티의 필터들을 통과할 수 있다.
-
-                // JWT를 만들 때 사용자 고유 ID로 삼았던 필드명과 맞춘다.
-                String userNameAttributeName = "sub";
-
-                List<GrantedAuthority> authorities = new ArrayList<>();
-                authorities.add(new SimpleGrantedAuthority(Role.USER.getKey()));
-
-                OAuth2User userDetails = new DefaultOAuth2User(authorities, attributes, userNameAttributeName);
-                OAuth2AuthenticationToken authentication = new OAuth2AuthenticationToken(userDetails, authorities, userNameAttributeName);
-
-                authentication.setDetails(userDetails);
-                SecurityContextHolder.getContext().setAuthentication(authentication);
-
-                request.setAttribute("member", jwtService.getMemberInfoFromToken(attributes));
             }
         }
 
